589b86ac7ca5d22e555cf7cced9c6904eb646155
AdminLavanderia <franco6cortez@gmail.com>
2025-11-21 18:35:18 -0300

    Update firebase.js

--- services/firebase.js
+++ services/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
 
 // Configuración de Firebase
 // (Tus credenciales están bien configuradas aquí)
